0e5171cec37b6a253056f4285adc69648376381a
Benato Denis <benato.denis96@gmail.com>
2018-07-08 19:35:51 +0200

    Problematic fuses fix....

--- Demos/Blink/main.c
+++ Demos/Blink/main.c
@@ -10,8 +10,6 @@
 #include "timers.h"   /* Software timer related API prototypes. */
 #include "semphr.h"   /* Semaphore related API prototypes. */
 
-static uint8_t led = 0xFF;
-
 /************************** Tasks ***************************/
 TaskHandle_t xBlinkTaskHandle = NULL;
 StaticTask_t xBlinkTaskBuffer;
@@ -21,31 +19,16 @@ void blinkTask(void* pvParameters);
 
 
 void appSetupHook(void) {
-    DDRB  |= _BV(DDB7);
-	PORTB |= _BV(PORTB7);       // Main (red PB7) LED on. Main LED on.
-    
-    while (1) {
-        _delay_ms(5000);
-        PINB  |= _BV(PINB7);       // Main (red PB7) LED toggle. Main LED fast blink.
-    }
-    
-	//xBlinkTaskHandle = xTaskCreateStatic(blinkTask, "Blink", sizeof(xBlinkTaskStack) / sizeof(StackType_t), NULL, configMAX_PRIORITIES - 2, xBlinkTaskStack, &xBlinkTaskBuffer);
+    DDRB  |= 0xFF;
+	xBlinkTaskHandle = xTaskCreateStatic(blinkTask, "Blink", sizeof(xBlinkTaskStack) / sizeof(StackType_t), NULL, configMAX_PRIORITIES - 2, xBlinkTaskStack, &xBlinkTaskBuffer);
 }
 
 void blinkTask(void* pvParameters __attribute__ ((unused))) {
-    
-    while (1) {
+    for (;;) {
         vTaskDelay(35000);
         
-        led = ~led;
-        
-        PORTA = led;
-        PORTB = led;
-        PORTC = led;
-        PORTD = led;
-        PORTF = led;
+        PORTB ^= 0xFF;
     }
-    
 }
 
 
@@ -53,11 +36,11 @@ void blinkTask(void* pvParameters __attribute__ ((unused))) {
 void appIdleHook(void) {
 	#if defined(__AVR_ATmega2560__) || defined(__AVR_ATmega2561__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega640__)
 	// before putting MCU to sleep interrupt MUST be enabled, or it won't wake up
-	sei();
+	//sei();
 	
 	// put the MCU to sleep, so that only a timer interrupt can wake it up
-	set_sleep_mode( SLEEP_MODE_IDLE );
-	sleep_mode();
+	//set_sleep_mode( SLEEP_MODE_IDLE );
+	//sleep_mode();
 	#endif
 }
 #endif
--- target.mk
+++ target.mk
@@ -1,11 +1,11 @@
 # ============= Target Specific ================
 # Change this file to meet project specification
 
-#MCU_TARGET 	= atmega328p
-MCU_TARGET      = atmega2560
+MCU_TARGET 	= atmega328p
+#MCU_TARGET      = atmega2560
 #MCU_TARGET     = atmega2561
 
-MCU_TARGET_AVRDUDE = m2560
+MCU_TARGET_AVRDUDE = m328p
 
 # DO NOT append UL after the H number
 CLOCK_HZ	= 16000000
@@ -42,14 +42,12 @@ SIZE 		= avr-size
 
 CC_FLAGS = -Wl,--gc-sections --std=gnu99 -ffunction-sections -fdata-sections -fpack-struct -fshort-enums -mcall-prologues -mrelax -g -Wall -O$(OPTIMIZATION_LEVEL) -mmcu=$(MCU_TARGET)
 
-# ============ Device fuse bits ================
-LFUSE = 0xc0
-HFUSE = 0x90
-EFUSE = 0xff
-
 # ============ Compilation rules ===============
 %.hex: %.elf
 	$(OBJCOPY) -j .text -j .data -O ihex $< $@
 
 %.eeprom: %.elf
 	$(OBJCOPY) -j .eeprom --change-section-lma .eeprom=0 -O ihex $< $@ 
+
+# append -u if you REALLY want to change fuses
+FLASH_CMD = $(AVRDUDE) -p $(MCU_TARGET_AVRDUDE) -c $(PROGRAMMER) -u
